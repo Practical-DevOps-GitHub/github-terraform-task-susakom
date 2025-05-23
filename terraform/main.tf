295e4be59a9f28f78eb0ee03f20e0eea1d8611d2
Oleksandr Susak <susak52@gmail.com>
2025-05-23 13:33:39 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,4 @@
+
 # ==============================
 # РЕСУРС: Управление существующим репозиторием
 # ==============================
@@ -9,23 +10,32 @@ resource "github_repository" "existing_repo" {
   has_projects = false
   has_wiki     = false
 
+  # Указываем, что default ветка — develop
   default_branch = "develop"
 }
 
-# Получаем ID репозитория для защиты веток
-data "github_repository" "existing_repo_data" {
-  name = github_repository.existing_repo.name
+# ==============================
+# РЕСУРС: Создание ветки develop (условное, если ветка существует данная часть пропуститься)
+# ==============================
+resource "github_branch" "develop_branch" {
+  count = var.create_develop_branch ? 1 : 0
+
+  repository = github_repository.existing_repo.name
+  branch     = "develop"
 }
+
+
 
 # ==============================
 # РЕСУРС: Защита ветки main
 # ==============================
 resource "github_branch_protection" "main_protection" {
-  repository_id = data.github_repository.existing_repo_data.id
-  pattern       = "main"
+  repository      = github_repository.existing_repo.name
+  branch          = "main"
 
-  required_pull_request_reviews {
-    require_code_owner_reviews  = true
+
+  required_pull_request_review {
+    require_code_owner_reviews  = true   # требуется апрув от владельца
     required_approving_count    = 1
   }
 }
@@ -34,20 +44,83 @@ resource "github_branch_protection" "main_protection" {
 # РЕСУРС: Защита ветки develop
 # ==============================
 resource "github_branch_protection" "develop_protection" {
-  repository_id = data.github_repository.existing_repo_data.id
-  pattern       = "develop"
+  repository      = github_repository.existing_repo.name
+  branch          = "develop"
 
-  required_pull_request_reviews {
+
+  required_pull_request_review {
     require_code_owner_reviews  = false
     required_approving_count    = 2
   }
 }
 
 # ==============================
+# РЕСУРС: Добавление секрета TERRAFORM
+# ==============================
+resource "github_actions_secret" "terraform_code" {
+  repository      = github_repository.existing_repo.name
+  secret_name     = "TERRAFORM"
+  plaintext_value = file("${path.module}/main.tf")
+}
+
+# ==============================
 # РЕСУРС: Добавление пользователя в репозиторий
 # ==============================
 resource "github_repository_collaborator" "add_user" {
+  repository       = github_repository.existing_repo.name
+  username         = "softservedata"
+  permission_level = "admin"
+}
+
+# ==============================
+# РЕСУРС: Добавление файла CODEOWNERS
+# ==============================
+resource "github_repository_file" "codeowners" {
   repository = github_repository.existing_repo.name
-  username   = "softservedata"
-  permission = "admin"
+  file       = ".github/CODEOWNERS"
+  content    = base64encode("* @softservedata")
+  branch     = "main"
+}
+resource "github_repository_file" "pull_request_template" {
+  repository = github_repository.existing_repo.name
+  file       = ".github/pull_request_template.md"
+  content    = base64encode("### Describe your changes\n\n<!-- Please describe what you've changed -->\n\n---\n\n### Issue ticket number and link\n\n<!-- For example: Closes #123 or https://github.com/your/repo/issues/123  -->\n\n---\n\n### Checklist before requesting a review\n\n- [ ] I have performed a self-review of my code\n- [ ] If it is a core feature, I have added thorough tests\n- [ ] Do we need to implement analytics?\n- [ ] Will this be part of a product update?\n<!-- If yes, please write one phrase about this update -->")
+  branch     = "develop"
+}
+
+
+# ==============================
+# РЕСУРС: Добавление Deploy Key
+# ==============================
+resource "github_repository_deploy_key" "deploy_key" {
+  repository = github_repository.existing_repo.name
+  title      = "DEPLOY_KEY"
+  key        = var.deploy_key_pub
+  read_only  = false
+}
+
+# ==============================
+# РЕСУРС: Сохраняем Discord Webhook как секрет
+# ==============================
+resource "github_actions_secret" "discord_webhook" {
+  repository      = github_repository.existing_repo.name
+  secret_name     = "DISCORD_WEBHOOK_URL"
+  plaintext_value = "https://discord.com/api/webhooks/1371418780156170290/kGb66wF5tigR-zVGhcsY2HFOc_2zzPc3pgLJMT81dMW6hMCx1sEkC-AY8sEQX0rVF9rX"
+}
+
+# ==============================
+# РЕСУРС: Добавляем GitHub Action для уведомлений в Discord
+# ==============================
+resource "github_repository_file" "discord_pr_notifier" {
+  repository = github_repository.existing_repo.name
+  file       = ".github/workflows/pull_request_discord_notify.yml"
+  content    = base64encode(file("${path.module}/templates/pull_request_discord_notify.yml"))
+  branch     = "main"
+}
+
+
+resource "github_actions_secret" "github_pat_secret" {
+  repository      = github_repository.existing_repo.name
+  secret_name     = "PAT"
+  plaintext_value = var.github_pat
 }
