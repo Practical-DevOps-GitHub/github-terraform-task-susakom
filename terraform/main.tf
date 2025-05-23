902e11a4eb6cdc8341aa159b5062875f4ef9ab90
Oleksandr Susak <susak52@gmail.com>
2025-05-23 13:51:57 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+
+
+
+
+
 
 # ==============================
 # РЕСУРС: Управление существующим репозиторием
