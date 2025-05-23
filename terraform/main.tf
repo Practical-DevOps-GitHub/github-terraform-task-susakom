b9ff1d70f0d6d352579fd81226f6157aa4a511d1
Oleksandr Susak <susak52@gmail.com>
2025-05-23 13:38:50 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,14 +24,17 @@ resource "github_branch" "develop_branch" {
   branch     = "develop"
 }
 
-
+# Получаем ID репозитория для защиты веток
+data "github_repository" "existing_repo_data" {
+  name = github_repository.existing_repo.name
+}
 
 # ==============================
 # РЕСУРС: Защита ветки main
 # ==============================
 resource "github_branch_protection" "main_protection" {
-  repository      = github_repository.existing_repo.name
-  branch          = "main"
+  repository_id = data.github_repository.existing_repo_data.id
+  pattern       = "main"
 
 
   required_pull_request_reviews {
@@ -44,11 +47,11 @@ resource "github_branch_protection" "main_protection" {
 # РЕСУРС: Защита ветки develop
 # ==============================
 resource "github_branch_protection" "develop_protection" {
-  repository      = github_repository.existing_repo.name
-  branch          = "develop"
+  repository      = data.github_repository.existing_repo_data.id
+  pattern          = "develop"
 
 
-  required_pull_request_review {
+  required_pull_request_reviews {
     require_code_owner_reviews  = false
     required_approving_count    = 2
   }
