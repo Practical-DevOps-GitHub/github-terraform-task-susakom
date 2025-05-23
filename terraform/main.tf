4be26e33eb5f86d8fd351ac64653b454deaeac95
Oleksandr Susak <susak52@gmail.com>
2025-05-23 16:16:45 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,7 +46,7 @@ data "github_repository" "existing_repo_data" {
 # РЕСУРС: Защита ветки main
 # ==============================
 resource "github_branch_protection" "main_protection" {
-  repository_id = data.github_repository.existing_repo.name.id
+  repository_id = github_repository.existing_repo.name.id
   pattern       = "main"
   required_pull_request_reviews {
     require_code_owner_reviews  = true   # требуется апрув от владельца
@@ -59,7 +59,7 @@ resource "github_branch_protection" "main_protection" {
 # РЕСУРС: Защита ветки develop
 # ==============================
 resource "github_branch_protection" "develop_protection" {
-  repository_id = data.github_repository.existing_repo.name.id
+  repository_id = github_repository.existing_repo.name.id
   pattern          = "develop"
 required_pull_request_reviews {
     require_code_owner_reviews  = false
