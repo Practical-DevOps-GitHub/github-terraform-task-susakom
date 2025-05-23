a1fcb5a28f9f1ab0496e238b5f50702492298347
Oleksandr Susak <susak52@gmail.com>
2025-05-23 23:01:41 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "github_branch" "develop_branch" {
 }
 
 resource "github_branch_default" "default_develop" {
-  repository_id = data.github_repository.existing_repo.id
+  repository = data.github_repository.existing_repo.name
   branch     = github_branch.develop_branch.branch
   depends_on = [github_branch.develop_branch]
 
