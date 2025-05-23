88d052d962dd1c06da253e692e246bb82d576a24
Oleksandr Susak <susak52@gmail.com>
2025-05-23 10:15:23 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,10 @@
+variable "github_pat" {
+  type        = string
+  description = "GitHub Personal Access Token"
+  sensitive   = true
+}
 
+variable "create_develop_branch" {
+  type    = bool
+  default = true
+}
