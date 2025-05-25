5046b4d06e9a583374d819384925b160cb9f4d66
Oleksandr Susak <susak52@gmail.com>
2025-05-25 17:58:07 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,6 +8,5 @@ variable "gitnub_pat" {
 variable "deploy_key_pub" {
   type        = string
   description = "SSH public key for deploy access"
-  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIj5doPA5fPI8+j21JGL9/j3F5b7shGX3Xlw4L+6rcWk deploy-key"
   sensitive   = true
 }
